8fd308c71c859c25479e502e2b104cb858a011c7
rgherdt <rgherdt@inf.ufrgs.br>
2014-08-22 11:20:19 -0300

    typed morphism

---
+++ src/TypedMorphism.hs
@@ -0,0 +1,22 @@
+{-# LANGUAGE TypeFamilies #-}
+
+module TypedMorphism
+    ( TypedMorphism
+    , TypedGraph
+    ) where
+
+import TypedMorphismClass
+import MorphismClass
+
+data TypedMorphism a b = TypedMorphism {
+                              getDomain   :: Morphism a b
+                            , getCodomain :: Morphism a b
+                            , getMapping  :: Morphism a b
+                         }
+
+instance TypedMorphismClass (TypedMorphism a b) where
+    type M (TypedMorphism a b) = Morphism a b
+
+    typedDomain m   = getDomain m
+    typedCodomain m = getCodomain m
+    typedMapping m  = getMapping m
