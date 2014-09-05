7c39914081626ee5697abd393bc07e2b0386bdd1
rgherdt <rgherdt@inf.ufrgs.br>
2014-09-05 13:23:18 -0300

    removed some parentheses; removed old code

--- src/GraphClass.hs
+++ src/GraphClass.hs
@@ -13,16 +13,16 @@ class GraphClass g where
   type Ed g :: *  -- edge type
 	
   -- Build and modify graphs
-  empty           ::                                    g  -- required
-  insertNode      ::                     (Nd g) -> g -> g  -- required
-  insertEdge      :: (Ed g) -> (Nd g) -> (Nd g) -> g -> g  -- required
-  removeNode      ::                     (Nd g) -> g -> g  -- required
-  removeEdge      ::                     (Ed g) -> g -> g  -- required
+  empty           ::                              g        -- required
+  insertNode      ::                 Nd g -> g -> g        -- required
+  insertEdge      :: Ed g -> Nd g -> Nd g -> g -> g        -- required
+  removeNode      ::                 Nd g -> g -> g        -- required
+  removeEdge      ::                 Ed g -> g -> g        -- required
 
   -- Test the presence and access nodes and edges
   nodes            :: g -> [Nd g]                          -- required
   edges            :: g -> [Ed g]                          -- required
-  nodesConnectedTo :: g -> (Ed g) -> [(Nd g, Nd g)]        -- required
+  nodesConnectedTo :: g -> Ed g -> [(Nd g, Nd g)]          -- required
   isNodeOf         :: Eq (Nd g) => g -> Nd g -> Bool
   isEdgeOf         :: Eq (Ed g) => g -> Ed g -> Bool
   sourceOf         :: g -> Ed g -> [Nd g]
@@ -59,36 +59,4 @@ class GraphClass g where
                             otherwise   -> False
   incidentEdges g n    = nub $ edgesIntoNode g n ++ edgesFromNode g n
   neighbourNodes g n   = nub $ nodesIntoNode g n ++ nodesFromNode g n 
-
-
--------------------------------------------------------------------------------------------
-
-{-
-class GraphClass g => TypedGraphClass g where
-
-  getTypeOfNode :: (Nd g) -> g -> Maybe (Nd g)
-  setTypeOfNode :: (Nd g) -> (Nd g) -> g -> g
-
-  getTypeOfEdge :: (Ed g) -> g -> Maybe (Ed g)
-  setTypeOfEdge :: (Ed g) -> (Ed g) -> g -> g
--}
-
-
-class GraphClass g => ValuedGraphClass g where
-
-  type NVal g :: *  -- node value
-  type EVal g :: *  -- edge value
-
-  setNodeValue   :: (Nd g) -> (NVal g) -> g -> g
-  getNodeValue   :: (Nd g) -> g -> Maybe (NVal g)
-  clearNodeValue :: (Nd g) -> g -> g
-  
-
-class GraphClass g => VisualGraphClass g where
-
-  getNodePos   :: (Nd g) -> g -> (Double,Double)
-  setNodePos   :: (Nd g) -> (Double,Double) -> g -> g
-  getEdgePos   :: (Ed g) -> g -> (Double,Double)
-  setEdgePos   :: (Ed g) -> (Double,Double) -> g -> g
-
 
