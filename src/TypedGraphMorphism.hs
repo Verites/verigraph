53d26d6a38b6b401fd711ae6dae9d44e99dd3071
rgherdt <rgherdt@inf.ufrgs.br>
2014-12-23 21:22:37 -0200

    TypedMorphism -> TypedGraphMorphism

--- src/TypedGraphMorphism.hs
+++ src/TypedGraphMorphism.hs
@@ -1,9 +1,9 @@
 {-# LANGUAGE TypeFamilies #-}
 
-module TypedMorphism (
+module TypedGraphMorphism (
       mapping
     , typedMorphism
-    , TypedMorphism
+    , TypedGraphMorphism
 ) where
 
 import Graph (Graph)
@@ -11,39 +11,39 @@ import GraphMorphism
 import Morphism
 import Valid
 
-data TypedMorphism a b = TypedMorphism {
+data TypedGraphMorphism a b = TypedGraphMorphism {
                               getDomain   :: GraphMorphism a b
                             , getCodomain :: GraphMorphism a b
                             , getMapping  :: GraphMorphism a b
                          }
 
-typedMorphism = TypedMorphism
+typedMorphism = TypedGraphMorphism
 mapping = getMapping
 
-instance (Eq a, Eq b) => Eq (TypedMorphism a b) where
-    (TypedMorphism dom1 cod1 m1) == (TypedMorphism dom2 cod2 m2) =
+instance (Eq a, Eq b) => Eq (TypedGraphMorphism a b) where
+    (TypedGraphMorphism dom1 cod1 m1) == (TypedGraphMorphism dom2 cod2 m2) =
         dom1 == dom2 &&
         cod1 == cod2 &&
         m1 == m2
 
-instance (Eq a, Eq b) => Morphism (TypedMorphism a b) where
-    type Obj (TypedMorphism a b) = GraphMorphism a b
+instance (Eq a, Eq b) => Morphism (TypedGraphMorphism a b) where
+    type Obj (TypedGraphMorphism a b) = GraphMorphism a b
 
     domain = getDomain
     codomain = getCodomain
     compose t1 t2 =
-        TypedMorphism (domain t1)
+        TypedGraphMorphism (domain t1)
                       (codomain t2)
                       $ compose (getMapping t1)
                                 (getMapping t2)
-    id t = TypedMorphism t t (Morphism.id $ domain t)
+    id t = TypedGraphMorphism t t (Morphism.id $ domain t)
     monomorphism = monomorphism . mapping
     epimorphism = epimorphism . mapping
     isomorphism = isomorphism . mapping
 
 
-instance (Eq a, Eq b) => Valid (TypedMorphism a b) where
-    valid (TypedMorphism dom cod m) =
+instance (Eq a, Eq b) => Valid (TypedGraphMorphism a b) where
+    valid (TypedGraphMorphism dom cod m) =
         valid dom &&
         valid cod &&
         dom == compose m cod
