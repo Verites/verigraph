6145b617761f17bb4c0ce84ca50df1996415c546
Ricardo G. Herdt <rgherdt@inf.ufrgs.br>
2015-01-09 00:12:19 -0200

    starting newNode

--- src/GUI.hs
+++ src/GUI.hs
@@ -1,11 +1,22 @@
 import Control.Monad.Trans.Class (lift)
-import Graph
+import qualified Graph as G
+import Data.IORef
+import qualified Data.Map as M
 import Graphics.UI.Gtk
 import Graphics.Rendering.Cairo
 import Graphics.UI.Gtk.Gdk.EventM
 
+type Coords = (Double, Double)
+
+data GrammarState = GrammarState {
+    initialGraph    :: G.Graph String String,
+    initialGraphPos :: M.Map String Coords,
+    counter         :: Int
+    }
+
 main = do
     initGUI
+    st <- newIORef $ GrammarState G.empty M.empty 0
     window <- windowNew
     set window [ windowTitle := "Verigraph" ]
     window `on` objectDestroy $ mainQuit
@@ -13,8 +24,8 @@ main = do
     canvas <- drawingAreaNew
     canvas `on` sizeRequest $ return (Requisition 40 40)
     ctxt <- cairoCreateContext Nothing
-    canvas `on` draw $ updateCanvas canvas
-    canvas `on` buttonPressEvent $ mouseClick
+    canvas `on` draw $ updateCanvas canvas st
+    canvas `on` buttonPressEvent $ mouseClick st
 
     containerAdd window canvas
 
@@ -23,8 +34,11 @@ main = do
     mainGUI 
 
 
-updateCanvas :: WidgetClass widget => widget -> Render ()
-updateCanvas canvas = do
+updateCanvas :: WidgetClass widget
+             => widget
+             -> IORef GrammarState
+             -> Render ()
+updateCanvas canvas st = do
     width'  <- liftIO $ widgetGetAllocatedWidth canvas
     height' <- liftIO $ widgetGetAllocatedHeight canvas
     let width = realToFrac width' / 2
@@ -35,13 +49,23 @@ updateCanvas canvas = do
     strokePreserve
     fill
 
-mouseClick :: EventM EButton Bool
-mouseClick = do
+mouseClick :: IORef GrammarState -> EventM EButton Bool
+mouseClick st = do
     button <- eventButton
-    liftIO $ case button of
-                LeftButton   -> putStrLn "LeftButton"
-                MiddleButton -> putStrLn "MiddleButton"
-                RightButton  -> putStrLn "RightButton"
-                otherwise    -> putStrLn "Unknown button"
+    click  <- eventClick
+    coords@(x, y) <- eventCoordinates
+    case (button, click) of
+        (LeftButton, DoubleClick) -> liftIO $ modifyIORef st (newNode coords)
+        otherwise                 -> liftIO $ putStrLn "Unknown button"
     return True
 
+newNode :: Coords -> GrammarState -> GrammarState
+newNode coords st =
+    GrammarState gr' pos (id + 1)
+  where
+    id = counter st
+    gr = initialGraph st
+    gr' = G.insertNode id gr
+    k = "i" ++ (show id)
+    pos = M.insert k coords $ initialGraphPos st
+    
