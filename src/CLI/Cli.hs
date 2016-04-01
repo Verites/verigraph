60b0d96a684e1d7695e6260dc1133d9bce99cefa
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2016-04-01 17:46:50 -0300

    Refactored Cli and options for calculating concurrent rules

--- src/CLI/Cli.hs
+++ src/CLI/Cli.hs
@@ -14,143 +14,34 @@ import qualified Text.XML.HXT.Core as HXT
 import qualified XML.GGXReader as XML
 import qualified XML.GGXWriter as GW
 
-data VerigraphOpts = Opts
-  { inputFile :: String
-  , outputFile :: Maybe String
-  , injectiveMatchesOnly :: Bool
-  , injectiveNacSatisfaction :: Bool
-  , analysis :: Analysis
-  , verbose :: Bool }
-
-data Analysis = Both | Conflicts | Dependencies | None deriving (Eq)
-
-verigraphOpts :: Parser VerigraphOpts
-verigraphOpts = Opts
-  <$> strArgument
-    ( metavar "INPUT_FILE"
-    <> action "file"
-    <> help "GGX file defining the graph grammar")
-  <*> optional (strOption
-    ( long "output-file"
-    <> short 'o'
-    <> metavar "FILE"
-    <> action "file"
-    <> help ("CPX file that will be written, receiving the critical pairs " ++
-             "for the grammar (if absent, a summary will be printed to stdout)")))
-  <*> flag False True
-    ( long "inj-matches-only"
-    <> help "Restrict the analysis to injective matches only")
-  <*> flag False True
-    ( long "inj-nac-satisfaction"
-    <> help ("Restrict the analysis of NAC satisfaction to injective " ++
-            "morphisms between the NAC graph and the instance graph"))
-  <*> (flag' Conflicts
-    ( long "conflicts-only"
-    <> help "Restrict to Critical Pair analysis")
-      <|> flag' Dependencies
-        ( long "dependencies-only"
-        <> help "Restrict to Critical Sequence analysis")
-        <|> pure Both)
-  <*> flag False True
-    ( long "verbose"
-    <> short 'v'
-    <> help "Print detailed information")
-
-calculateConflicts :: Analysis -> Bool
-calculateConflicts flag = flag `elem` [Both,Conflicts]
-
-calculateDependencies :: Analysis -> Bool
-calculateDependencies flag = flag `elem` [Both,Dependencies]
-
-execute :: VerigraphOpts -> IO ()
-execute opts = do
-    gg <- XML.readGrammar (inputFile opts)
-    ggName <- XML.readGGName (inputFile opts)
-    names <- XML.readNames (inputFile opts)
-
-    putStrLn "Analyzing the graph grammar..."
-    putStrLn ""
-
-    let nacInj = injectiveNacSatisfaction opts
-        onlyInj = injectiveMatchesOnly opts
-        action = analysis opts
-        writer = defWriterFun nacInj onlyInj action
-        rules = map snd (GG.rules gg)
-        puMatrix = pairwiseCompare (allProduceUse nacInj onlyInj) rules
-        ddMatrix = pairwiseCompare (allDeliverDelete nacInj onlyInj) rules
-        udMatrix = pairwiseCompare (allDeleteUse nacInj onlyInj) rules
-        pfMatrix = pairwiseCompare (allProduceForbid nacInj onlyInj) rules
-        peMatrix = pairwiseCompare (allProdEdgeDelNode nacInj onlyInj) rules
-        conflictsMatrix = liftMatrix3 (\x y z -> x ++ y ++ z) udMatrix pfMatrix peMatrix
-        dependenciesMatrix = liftMatrix2 (\x y -> x ++ y) puMatrix ddMatrix
-
-        conflicts = [ "Delete-Use:"
-                , show (length <$> udMatrix)
-                , ""
-                , "Produce-Forbid:"
-                , show (length <$> pfMatrix)
-                , ""
-                , "Produce Edge Delete Node:"
-                , show (length <$> peMatrix)
-                , "All Conflicts:"
-                , show (length <$> conflictsMatrix)
-                , ""]
-
-        dependencies = [ "Produce Use Dependency:"
-                   , show (length <$> puMatrix)
-                   , ""
-                   , "Deliver Delete Dependency:"
-                   , show (length <$> ddMatrix)
-                   , ""
-                   , "All Dependencies:"
-                   , show (length <$> dependenciesMatrix)
-                   , ""]
-
-    -- FIXME handle concurrent rules properly
-    sequences <- XML.readSequences gg (inputFile opts)
-    let newRules = map (\(name, rules) -> (name, maxConcurrentRule rules)) sequences
-        gg' = GG.graphGrammar (GG.initialGraph gg) (GG.rules gg ++ newRules)
-    print $ map (fst) newRules
-    print $ map (nacs . snd) newRules
-    GW.writeGrammarFile gg' ggName names "concurrent.ggx"
-
-    case outputFile opts of
-      Just file -> writer gg ggName names file
-      Nothing -> mapM_
-                 putStrLn $
-                 (if calculateConflicts action then conflicts else [])
-                 ++ (if calculateDependencies action then dependencies else [])
-                 ++ ["Done!"]
-
-defWriterFun :: Bool -> Bool -> Analysis
-             ->(GG.GraphGrammar a b -> String
-             -> [(String,String)] -> String -> IO ())
-defWriterFun nacInj inj t = case t of
-                   Conflicts    -> GW.writeConflictsFile nacInj inj
-                   Dependencies -> GW.writeDependenciesFile nacInj inj
-                   Both         -> GW.writeConfDepFile nacInj inj
-                   None         -> GW.writeGrammarFile
-
--- | Combine three matrices with the given function. All matrices _must_ have
--- the same dimensions.
-liftMatrix3 :: (a -> b -> c -> d) -> Matrix a -> Matrix b -> Matrix c -> Matrix d
-liftMatrix3 f ma mb mc = matrix (nrows ma) (ncols ma) $ \pos ->
-  f (ma!pos) (mb!pos) (mc!pos)
-
--- | Combine two matrices with the given function. All matrices _must_ have
--- the same dimensions.
-liftMatrix2 :: (a -> b -> c) -> Matrix a -> Matrix b -> Matrix c
-liftMatrix2 f ma mb = matrix (nrows ma) (ncols ma) $ \pos ->
-  f (ma!pos) (mb!pos)
-
-pairwiseCompare :: (a -> a -> b) -> [a] -> Matrix b
-pairwiseCompare compare items =
-  matrix (length items) (length items) $ \(i,j) ->
-    compare (items !! (i-1)) (items !! (j-1))
+import CLI.GlobalOptions
+import qualified CLI.CriticalPairAnalysis as CPA
+import qualified CLI.ConcurrentRules as CR
 
 main :: IO ()
 main = execParser opts >>= execute
   where
-    opts = info (helper <*> verigraphOpts)
+    opts = info (helper <*> options)
       ( fullDesc
-      <> progDesc "Run critical pair analysis on a given graph grammar.")
+      <> progDesc "Software specification and verification tool based on graph rewriting")
+
+execute :: (GlobalOptions, Command) -> IO ()
+execute (globalOpts, CPAnalysis opts) = CPA.runCPAnalysis globalOpts opts
+execute (globalOpts, ConcurrentRules opts) = CR.runConcurrentRules globalOpts opts
+
+options :: Parser (GlobalOptions, Command)
+options = (\cmd opts -> (opts, cmd)) <$> commands <*> globalOpts
+
+data Command =
+    CPAnalysis CPA.CPOpts
+  | ConcurrentRules CR.CROpts
+
+commands :: Parser Command
+commands = subparser (cpAnalysis <> concurrentRule)
+  where
+    cpAnalysis = command "analysis" . fmap CPAnalysis $ info (helper <*> CPA.cpOpts)
+      ( fullDesc <> progDesc "Run critical pair analysis on the input grammar")
+
+    concurrentRule = command "concurrent-rule" . fmap ConcurrentRules $ info (helper <*> CR.crOpts)
+      ( fullDesc <> progDesc "Generate concurrent rules for the input grammar and rule sequences")
+
---
+++ src/CLI/GlobalOptions.hs
@@ -0,0 +1,36 @@
+module CLI.GlobalOptions
+  ( GlobalOptions
+  , globalOpts
+
+  , injectiveMatchesOnly
+  , injectiveNacSatisfaction
+  , verbose
+  , inputFile
+  ) where
+
+import           Options.Applicative
+
+data GlobalOptions = GOpts
+  { injectiveMatchesOnly :: Bool
+  , injectiveNacSatisfaction :: Bool
+  , verbose :: Bool
+  , inputFile :: String
+  }
+
+globalOpts :: Parser GlobalOptions
+globalOpts = GOpts
+  <$> flag False True
+    ( long "inj-matches-only"
+    <> help "Restrict the analysis to injective matches only")
+  <*> flag False True
+    ( long "inj-nac-satisfaction"
+    <> help ("Restrict the analysis of NAC satisfaction to injective " ++
+            "morphisms between the NAC graph and the instance graph"))
+  <*> flag False True
+    ( long "verbose" <> short 'v')
+  <*> strArgument
+    ( metavar "INPUT_FILE"
+    <> action "file"
+    <> help "GGX file defining the graph grammar")
+
+
