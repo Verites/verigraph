a890bdee491be6bb9b267d0bedc273d7b2c6c359
andrei529 <acosta@inf.ufpel.edu.br>
2016-04-07 16:17:26 -0300

    Added commutingPairs to EpiPairs

--- src/Analysis/EpiPairs.hs
+++ src/Analysis/EpiPairs.hs
@@ -1,6 +1,8 @@
 module Analysis.EpiPairs
- (createPairs) where
+ ( createPairs
+ , commutingPairs) where
 
+import           Abstract.Morphism        (compose)
 import           Analysis.GPToVeri        (mountTGMBoth)
 import           Analysis.GraphPart       (genEqClass)
 import           Analysis.VeriToGP        (mixTGM)
@@ -11,3 +13,12 @@ createPairs :: TypedGraphMorphism a b
      -> TypedGraphMorphism a b
      -> [(TypedGraphMorphism a b, TypedGraphMorphism a b)]
 createPairs m1 m2 = map (mountTGMBoth m1 m2) (genEqClass (mixTGM m1 m2))
+
+-- | Create all jointly surjective pairs of @m1@ and @m2@ that commutes, considering they have same domain
+commutingPairs :: TypedGraphMorphism a b
+     -> TypedGraphMorphism a b
+     -> [(TypedGraphMorphism a b, TypedGraphMorphism a b)]
+commutingPairs m1 m2 = filt
+  where
+    allPairs = createPairs m1 m2
+    filt = filter (\(x,y) -> compose m1 x == compose m2 y) allPairs
