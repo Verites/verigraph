57d7761721dbc6f4daf52413b02e3a09086d3771
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2016-04-13 12:23:01 -0300

    Extracted type class for epi pairs

--- src/Analysis/EpiPairs.hs
+++ src/Analysis/EpiPairs.hs
@@ -1,29 +1,46 @@
-module Analysis.EpiPairs
- ( createPairs
- , createPairsCodomain
- , commutingPairs) where
+module Analysis.EpiPairs (EpiPairs(..)) where
 
-import           Abstract.Morphism        (codomain,compose)
+import           Abstract.Morphism        (Morphism, Obj, codomain, compose)
 import           Analysis.GPToVeri        (mountTGMBoth)
 import           Analysis.GraphPart       (genEqClass)
 import           Analysis.VeriToGP        (mixTGM)
 import           Graph.TypedGraphMorphism (TypedGraphMorphism)
-import           Graph.GraphMorphism      (GraphMorphism)
+import           Graph.GraphMorphism      (TypedGraph)
 
--- | Create all jointly surjective pairs of @m1@ and @m2@
-createPairs :: GraphMorphism a b -> GraphMorphism a b
-     -> [(TypedGraphMorphism a b, TypedGraphMorphism a b)]
-createPairs m1 m2 = map (mountTGMBoth m1 m2) (genEqClass (mixTGM m1 m2))
+class Morphism m => EpiPairs m where
+  -- | Create all jointly epimorphic pairs of morphisms from the given objects.
+  createPairs :: Obj m -> Obj m -> [(m, m)]
 
--- | Create all jointly surjective pairs of @m1@ and @m2@
-createPairsCodomain :: TypedGraphMorphism a b -> TypedGraphMorphism a b
-     -> [(TypedGraphMorphism a b, TypedGraphMorphism a b)]
-createPairsCodomain m1 m2 = createPairs (codomain m1) (codomain m2)
+  -- | Create all jointly epimorphic pairs of morphisms from the codomains of
+  -- the given morphisms.
+  createPairsCodomain :: m -> m -> [(m, m)]
+  createPairsCodomain m1 m2 = createPairs (codomain m1) (codomain m2)
 
--- | Create all jointly surjective pairs of @m1@ and @m2@ that commutes, considering they have same domain
-commutingPairs :: TypedGraphMorphism a b -> TypedGraphMorphism a b
-     -> [(TypedGraphMorphism a b, TypedGraphMorphism a b)]
-commutingPairs m1 m2 = filt
-  where
-    allPairs = createPairs (codomain m1) (codomain m2)
-    filt = filter (\(x,y) -> compose m1 x == compose m2 y) allPairs
+  -- | Given two morphisms from the same domain, create all jointly epimorphic
+  -- pairs of morphisms from their codomains, such that the square formed by
+  -- all these morphisms commutes.
+  --
+  -- Given /f : X -> A/ and /g : X -> B/, obtain all jointly epimorphic pairs
+  -- /(f', g')/ such that the following diagram commutes.
+  --
+  -- @
+  --       g
+  --    X----->B
+  --    |      |
+  --  f |      | f'
+  --    v      v
+  --    A----->Y
+  --       g'
+  -- @
+  commutingPairs :: m -> m -> [(m, m)]
+
+
+instance EpiPairs (TypedGraphMorphism a b) where
+  -- | Create all jointly surjective pairs of @m1@ and @m2@
+  createPairs m1 m2 = map (mountTGMBoth m1 m2) (genEqClass (mixTGM m1 m2))
+
+  -- | Create all jointly surjective pairs of @m1@ and @m2@ that commutes, considering they have same domain
+  commutingPairs m1 m2 = filt
+    where
+      allPairs = createPairs (codomain m1) (codomain m2)
+      filt = filter (\(x,y) -> compose m1 x == compose m2 y) allPairs
