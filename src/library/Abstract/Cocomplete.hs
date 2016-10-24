cedf954ab9ded808f36760b030f015f645ae0b4a
Jonas S. Bezerra <jonassantosbezerra@gmail.com>
2016-10-24 21:58:03 -0200

    Updating docs

--- src/library/Abstract/Cocomplete.hs
+++ src/library/Abstract/Cocomplete.hs
@@ -9,7 +9,9 @@ where
 import           Abstract.Morphism
 
 class (Morphism m) => Cocomplete m where
-  -- | f : A -> B, g : A -> B, h : B -> X
+
+  -- | Given two morphisms @/f : A -> B/@ and @/g : A -> B/@ retuns the coequalizer morphism
+  -- @/h : B -> X/@
   calculateCoequalizer :: m -> m -> m
 
   --calculateCoproduct :: m -> m -> (m,m)
