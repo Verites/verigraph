ca0e07ac60656ada775cdbbacf9b84402edf1752
Jonas S. Bezerra <jsbezerra@inf.ufrgs.br>
2016-11-03 18:13:00 -0200

    Base case of graph process construction
    Base case of graph process construction

--- src/Main.hs
+++ src/Main.hs
@@ -30,6 +30,9 @@ import           Abstract.DPO                   as RW
 import qualified Data.List                      as L
 import           Abstract.Cardinality
 import           Data.Maybe
+import Abstract.DPO.Derivation
+import Abstract.DPO.Process
+import TypedGraph.GraphProcess
 
 --import qualified XML.GGXReader as XML
 
@@ -154,13 +157,11 @@ tmh6 = buildTypedGraphMorphism ta6 tb6 mh6
 
 teste60 = calculateNCoequalizer $ NE.fromList [tmf6]
 
-{--
+
 {-sendMSG-}
 lr1 = build [11,13,14] [(11,13,11)]
 kr1 = build [21,23,24] []
 rr1 = build [31,33,34] [(35,33,34)]
-
-
 
 --tipagem
 tlr1 = GM.buildGraphMorphism lr1 grafotipo [(14,4),(13,3),(11,1)] [(11,1)]
@@ -174,6 +175,25 @@ l1 = buildTypedGraphMorphism tkr1 tlr1 kr1_lr1
 kr1_rr1 = GM.buildGraphMorphism kr1 rr1 [(24,34),(23,33),(21,31)] []
 r1 = buildTypedGraphMorphism tkr1 trr1 kr1_rr1
 
+graph' = build [1,2,3,4] [(1,2,1)]
+graph'' = GM.buildGraphMorphism graph' grafotipo [(3,4),(2,3),(1,1),(4,2)] [(1,1)]
+
+meu :: GM.GraphMorphism a b -> GM.GraphMorphism a b -> [TypedGraphMorphism a b]
+meu = findMonomorphisms
+
+myconfig = DPOConfig MonoMatches MonomorphicNAC
+
+meumatch = head $ meu tlr1 graph''
+
+myrule = buildProduction l1 r1 []
+
+der = generateDerivation myconfig meumatch myrule
+getDer = fromJust der
+myprocess = singleProcess getDer
+myps = productions myprocess
+myc = coreObject myprocess
+
+{--
 --nac
 nacGraph = build [501,502,503,504] [(501,503,501),(503,502,503)]
 nacType = GM.buildGraphMorphism nacGraph grafotipo [(501,1),(502,2),(503,3),(504,4)] [(501,1),(503,3)]
---
+++ src/library/TypedGraph/GraphProcess.hs
@@ -0,0 +1,34 @@
+module TypedGraph.GraphProcess (
+singleProcess
+                               )
+
+where
+
+import Abstract.Cocomplete
+import Abstract.DPO
+import Abstract.DPO.Process
+import Abstract.DPO.Derivation
+import Abstract.Morphism
+import TypedGraph.Morphism
+
+singleProcess :: Derivation (TypedGraphMorphism a b) -> Process (TypedGraphMorphism a b)
+singleProcess derivation =
+  let d1 = dToG derivation
+      d2 = dToH derivation
+      (d1',d2') = calculatePushout d1 d2
+      core = codomain d1'
+      rule = production derivation
+      oldL = getLHS rule
+      oldR = getRHS rule
+      mappingL = mapping oldL
+      mappingR = mapping oldR
+      m = match derivation
+      h = comatch derivation
+      newLType = compose (mapping m) (mapping d2')
+      newRType = compose (mapping h) (mapping d1')
+      newKType = compose (mappingL) newLType
+      newL = buildTypedGraphMorphism newKType newLType mappingL
+      newR = buildTypedGraphMorphism newKType newRType mappingR
+      newProduction = buildProduction newL newR []
+   in Process [newProduction] core
+
