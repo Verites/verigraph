193906c3c134cd807239ad53a17c9f66da455b5e
Jonas S. Bezerra <jsbezerra@inf.ufrgs.br>
2016-11-04 12:26:17 -0200

    Applying hlint

--- src/library/Abstract/DPO/Derivation.hs
+++ src/library/Abstract/DPO/Derivation.hs
@@ -21,6 +21,8 @@ generateDerivationUnsafe match rule = Derivation rule match n k f g
   where
     (k,n,f,g) = calculateDPO match rule
 
+
+-- | Given a match @m@ and a production @p@, it returns @Just d@, where @d@ is the corresponding Derivation if @m@ satisfies the rewriting conditions, or @Nothing@.
 generateDerivation :: (DPO m) => DPOConfig -> m -> Production m -> Maybe (Derivation m)
 generateDerivation conf match rule =
   if satisfiesRewritingConditions conf rule match then
--- src/library/Abstract/DPO/Process.hs
+++ src/library/Abstract/DPO/Process.hs
@@ -1,31 +1,13 @@
 module Abstract.DPO.Process (
   Process(..)
-, firstAtempt
                             )
 
 where
 
-import Abstract.Cocomplete
 import Abstract.DPO
-import Abstract.DPO.Derivation
 import Abstract.Morphism
 
 data Process m = Process
   { productions :: [Production m]
   , coreObject :: Obj m
   }
-
-firstAtempt :: (DPO m) => Derivation m -> Process m
-firstAtempt derivation =
-  let d1 = dToG derivation
-      d2 = dToH derivation
-      (d1', d2') = calculatePushout d1 d2
-      core = codomain d1'
-      oldL = getLHS $ production derivation
-      oldR = getRHS $ production derivation
-      newL = compose d1' (compose oldL (match derivation))
-      newR = compose d2' (compose oldR (comatch derivation))
-      newProduction = buildProduction newL newR []
-  in Process [newProduction] core
-
-
