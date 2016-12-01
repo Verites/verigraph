099c8fa2a2af86df3a1ccf0da5c4341eab7024f8
Jonas S. Bezerra <jsbezerra@inf.ufrgs.br>
2016-12-01 17:01:06 -0200

    Cleaning unused code

--- src/library/Abstract/DPO/Derivation.hs
+++ src/library/Abstract/DPO/Derivation.hs
@@ -1,10 +1,6 @@
 module Abstract.DPO.Derivation
 ( Derivation(..)
 , generateDerivation
-, getKObjects
-, getAllObjects
-, getLHSs
-, getRHSs
 , getDObjects
 , getAllBottomObjects
 , getLefts
@@ -30,34 +26,12 @@ generateDerivationUnsafe match rule = Derivation rule match n k f g
   where
     (k,n,f,g) = calculateDPO match rule
 
-
 -- | Given a match @m@ and a production @p@, it returns @Just d@, where @d@ is the corresponding Derivation if @m@ satisfies the rewriting conditions, or @Nothing@.
 generateDerivation :: (DPO m) => DPOConfig -> m -> Production m -> Maybe (Derivation m)
 generateDerivation conf match rule =
   if satisfiesRewritingConditions conf rule match then
      Just (generateDerivationUnsafe match rule)
   else Nothing
-
-getKObjects :: (DPO m) =>  [Derivation m] -> [Obj m]
-getKObjects = fmap (domain . getLHS . production)
-
-
-
-getLHSs :: [Derivation m] -> [m]
-getLHSs = fmap (getLHS . production)
-
---teste :: (DPO m) => [Derivation m] -> [m]
---teste = map (\d -> compose ((getLHS . production) d) (match d))
-
-getRHSs :: [Derivation m] -> [m]
-getRHSs = fmap (getRHS . production)
-
-getAllObjects :: (DPO m) => Derivation m -> [Obj m]
-getAllObjects ds =
-  let l = codomain . getLHS . production
-      k =   domain . getLHS . production
-      r = codomain . getRHS . production
-   in [l ds, k ds, r ds]
 
 getDObjects :: (DPO m) =>  [Derivation m] -> [Obj m]
 getDObjects = fmap (domain . dToG)
