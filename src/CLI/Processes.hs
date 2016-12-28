7dd8f4c13e50ad5d687170b87818d608527a5d9f
Jonas S. Bezerra <jsbezerra@inf.ufrgs.br>
2016-12-28 17:18:11 -0200

    Starting CLI interface for graph processes

---
+++ src/CLI/Processes.hs
@@ -0,0 +1,57 @@
+module Processes
+  ( Options
+  , options
+  , execute
+  ) where
+
+import           GlobalOptions
+
+import           Abstract.AdhesiveHLR
+import           Analysis.ConcurrentRules
+import           Control.Monad
+import qualified Grammar.Core        as GG
+import           Options.Applicative
+import           TypedGraph.DPO.GraphRule
+import           TypedGraph.Morphism
+import qualified XML.GGXReader            as XML
+import qualified XML.GGXWriter            as GW
+
+data Options = Options
+  { outputFile     :: String }
+
+--data CREpiPairsType = AllEpiPairs | OnlyInjectiveEpiPairs deriving (Eq)
+
+options :: Parser Options
+options = Options
+  <$> strOption
+    ( long "output-file"
+    <> short 'o'
+    <> metavar "FILE"
+    <> action "file"
+    <> help "GGX file that will be written, adding the concurrent rules to the original graph grammar")
+
+execute :: GlobalOptions -> Options -> IO ()
+execute globalOpts opts = do
+    let dpoConf = morphismsConf globalOpts
+
+    (gg,gg2,_) <- XML.readGrammar (inputFile globalOpts) (useConstraints globalOpts) dpoConf
+    ggName <- XML.readGGName (inputFile globalOpts)
+    names <- XML.readNames (inputFile globalOpts)
+    sequences <- XML.readSequences gg (inputFile globalOpts)
+    {-let makeConcurrentRules = case generationType opts of
+                                MaxConcurrentRule  -> makeMaxConcurrentRules
+                                AllConcurrentRules -> makeAllConcurrentRules
+        dependencies = concRulesbyDep opts
+        newRules = map (makeConcurrentRules dependencies (morphismsConf globalOpts) (GG.constraints gg)) sequences
+
+    forM_ (zip sequences newRules) $ \((name, _, _), rules) ->
+      when (null rules)
+        (putStrLn $ "No concurrent rules were found for rule sequence '" ++ name ++ "'")
+-}
+    let gg' = GG.grammar (GG.start gg) [] (GG.rules gg)
+    GW.writeGrammarFile (gg',gg2) ggName names (outputFile opts)
+
+
+makeAllConcurrentRules :: CRDependencies -> MorphismsConfig -> [Constraint (TypedGraphMorphism a b)] -> (String, [GraphRule a b], [c]) -> [(String, GraphRule a b)]
+makeAllConcurrentRules dep conf constraints (baseName, sequence, _) = zipWith makeName (allConcurrentRules dep conf constraints sequence) [0::Int ..]
+  where makeName rule idx = (baseName++"_"++show idx, rule)
