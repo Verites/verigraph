a4b486f5910ea60b940b6ce61a3b7b01b428279c
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2017-04-12 14:04:46 -0300

    Document module SymbolicGraph

--- src/library/SymbolicGraph.hs
+++ src/library/SymbolicGraph.hs
@@ -1,8 +1,21 @@
+{-|
+An implementation of symbolic graphs.
+
+Symbolic graphs are graphs with variables as attributes, accompanied by a set of restrictions that
+describes the possible values of these variables. The variables and restrictions are interpreted
+with respect to the algebra of integers with addition, subtraction and multiplication.
+
+The implementation is based on the @Graph.Graph@ module. Currently, it only supports untyped graphs
+where nodes have a single attribute.
+
+Operation comments contain the operation time complexity in the Big-O notation
+<http://en.wikipedia.org/wiki/Big_O_notation>, denoting by /v/ the number of nodes in a graph, and
+by /e/ the number of edges in a graph.
+-}
 module SymbolicGraph
   (
   -- * Symbolic Graph type
     SymbolicGraph
-  , restrictions
   , NodeId
   , Node
   , node
@@ -24,7 +37,7 @@ module SymbolicGraph
   , outgoingEdges
 
   -- * Query
-  , SymbolicGraph.Internal.null
+  , isEmpty
   , isNodeOf
   , isEdgeOf
   , lookupNode
@@ -47,18 +60,17 @@ module SymbolicGraph
 
   -- * Update
   , updateNodeAttribute
+  , updateNodeAttributes
 
   -- * Conversion
   -- ** Lists
   , nodes
   , edges
+  , restrictions
   , nodeIds
   , edgeIds
   , nodesInContext
   , edgesInContext
-
-  -- * Map
-  , mapNodeAttributes
   ) where
 
 import           SymbolicGraph.Internal
--- src/library/SymbolicGraph/Internal.hs
+++ src/library/SymbolicGraph/Internal.hs
@@ -22,15 +22,15 @@ module SymbolicGraph.Internal
   , targetId
 
   -- * Contexts and graph traversal
+  , NodeContext
   , NodeInContext
   , EdgeInContext
-  , NodeContext
   , incidentEdges
   , incomingEdges
   , outgoingEdges
 
   -- * Query
-  , SymbolicGraph.Internal.null
+  , isEmpty
   , isNodeOf
   , isEdgeOf
   , lookupNode
@@ -53,6 +53,7 @@ module SymbolicGraph.Internal
 
   -- * Update
   , updateNodeAttribute
+  , updateNodeAttributes
 
   -- * Conversion
   -- ** Lists
@@ -62,9 +63,6 @@ module SymbolicGraph.Internal
   , edgeIds
   , nodesInContext
   , edgesInContext
-
-  -- * Map
-  , mapNodeAttributes
   ) where
 
 import           SymbolicGraph.DataAlgebra
@@ -82,10 +80,21 @@ type InnerGraph =
   Graph (Maybe Variable) ()
 
 
+-- | A graph with variables as attributes, as well a set of restrictions constraining the possible
+-- values of these variables.
+--
+-- Every node and edge is identified by a unique integer. The "namespaces" of nodes and edges are
+-- independent, i.e. the same number may be used to identify both a node and an edge within the
+-- same graph.
+--
+-- Equality tests cost /O(v² + e²)/.
+--
+-- __Note:__ valid symbolic graphs may have unsatisfiable restrictions.
 data SymbolicGraph =
   SymbGraph
     { graphPart    :: InnerGraph
     , restrictions :: [Restriction]
+      -- ^ List of restrictions of the graph, constraining the values that its variables may take. /O(1)/.
     }
   deriving Eq
 
@@ -96,10 +105,12 @@ modifyGraphPart f graph =
   graph { graphPart = f (graphPart graph) }
 
 
+-- | Nodes from within a symbolic graph, also containing their attribute.
 newtype Node =
   Node { unNode :: Graph.Node (Maybe Variable) }
 
 
+-- | Construct a node. Used to insert nodes into symbolic graphs.
 node :: NodeId -> Maybe Variable -> Node
 node n v =
   Node (Graph.Node n v)
@@ -115,10 +126,12 @@ nodeAttribute =
   Graph.nodeInfo . unNode
 
 
+-- | Edges from within a symbolic graph, also containing their source and target.
 newtype Edge =
   Edge { unEdge :: Graph.Edge () }
 
 
+-- | Construct an edge. Used to insert edges into symbolic graphs.
 edge :: EdgeId -> NodeId -> NodeId -> Edge
 edge e src tgt =
   Edge (Graph.Edge e src tgt ())
@@ -193,30 +206,46 @@ instance Valid SymbolicGraph where
 
 
 -- * Context and graph traversal
+{- $contexts
+See the discussion of contexts and graph traversal in module 'Graph.Graph'.
+-}
 
 
-type NodeInContext =
-  (Node, NodeContext)
-
-
-type EdgeInContext =
-  (NodeInContext, Edge, NodeInContext)
-
-
+-- | Provides access to a node's incident edges.
 newtype NodeContext =
   NodeCtx { unNodeCtx :: Graph.NodeContext (Maybe Variable) () }
 
 
+-- | Shorthand for having a node along with its context.
+type NodeInContext =
+  (Node, NodeContext)
+
+
+-- | Shorthand for having an edge along with its source and target in context.
+--
+-- Because of lazyness, constructing a value of this type does __not__ evaluate the node lookup.
+-- Thus, forcing the evaluation of the nodes costs /O(v)/. Keep this in mind when using values
+-- of this type.
+type EdgeInContext =
+  (NodeInContext, Edge, NodeInContext)
+
+
+-- | Get the edges that are incident on the current node.
+-- /O(e)/, plus the cost of evaluating the nodes of the result (see 'EdgeInContext').
 incidentEdges :: NodeContext -> [EdgeInContext]
 incidentEdges =
   map wrapEdgeInContext . Graph.incidentEdges . unNodeCtx
 
 
+-- | Get the edges that have the current node as target.
+-- /O(e)/, plus the cost of evaluating the nodes of the result (see 'EdgeInContext').
 incomingEdges :: NodeContext -> [EdgeInContext]
 incomingEdges =
   map wrapEdgeInContext . Graph.incomingEdges . unNodeCtx
 
 
+-- | Get the edges that have the current node as source.
+-- /O(e)/, plus the cost of evaluating the nodes of the result (see 'EdgeInContext').
 outgoingEdges :: NodeContext -> [EdgeInContext]
 outgoingEdges =
   map wrapEdgeInContext . Graph.outgoingEdges . unNodeCtx
@@ -237,36 +266,44 @@ wrapEdgeInContext (src, e, tgt) =
 -- * Query
 
 
-null :: SymbolicGraph -> Bool
-null (SymbGraph graph restrictions) =
-  Graph.null graph && Set.null (freeVariablesOf restrictions)
+-- | True if the given graph has no nodes, edges or restrictions. /O(1)/.
+isEmpty :: SymbolicGraph -> Bool
+isEmpty (SymbGraph graph restrictions) =
+  Graph.null graph && Prelude.null restrictions
 
 
+-- | True if there is a node with given identifier in the given graph. /O(v)/.
 isNodeOf :: NodeId -> SymbolicGraph -> Bool
 isNodeOf nodeId graph =
   Graph.isNodeOf (graphPart graph) nodeId
 
 
+-- | True if there is an edge with given identifier in the given graph. /O(e)/.
 isEdgeOf :: EdgeId -> SymbolicGraph -> Bool
 isEdgeOf edgeId graph =
   Graph.isEdgeOf (graphPart graph) edgeId
 
 
+-- | Look up the node with given identifier in the graph. /O(v)/.
 lookupNode :: NodeId -> SymbolicGraph -> Maybe Node
 lookupNode nodeId graph =
   Node <$> Graph.lookupNode nodeId (graphPart graph)
 
 
+-- | Look up the node with given identifier, along with its context, in the graph. /O(v)/.
 lookupNodeInContext :: NodeId -> SymbolicGraph -> Maybe NodeInContext
 lookupNodeInContext nodeId graph =
   wrapNodeInContext <$> Graph.lookupNodeInContext nodeId (graphPart graph)
 
 
+-- | Look up the edge with given identifier in the graph. /O(e)/.
 lookupEdge :: EdgeId -> SymbolicGraph -> Maybe Edge
 lookupEdge edgeId graph =
   Edge <$> Graph.lookupEdge edgeId (graphPart graph)
 
 
+-- | Look up the edge with given identifier, along with its context, in the graph.
+-- /O(e)/, plus the cost of evaluating the nodes of the result (see 'EdgeInContext').
 lookupEdgeInContext :: EdgeId -> SymbolicGraph -> Maybe EdgeInContext
 lookupEdgeInContext edgeId graph =
   wrapEdgeInContext <$> Graph.lookupEdgeInContext edgeId (graphPart graph)
@@ -276,11 +313,16 @@ lookupEdgeInContext edgeId graph =
 -- * Construction
 
 
+-- | A symbolic graph with no nodes, edges or restrictions.
 empty :: SymbolicGraph
 empty =
   SymbGraph Graph.empty []
 
 
+-- | Build a graph from lists of nodes, edges and restrictions. Edges with undefined source or
+-- target are ignored and omitted from the resulting graph. /O(v + e*v)/.
+--
+-- /Note:/ the restrictions of the resulting graph may be unsatisfiable.
 fromNodesAndEdges :: [Node] -> [Edge] -> [Restriction] -> SymbolicGraph
 fromNodesAndEdges nodes edges restrictions =
   let
@@ -294,11 +336,15 @@ fromNodesAndEdges nodes edges restrictions =
 -- * Insertion
 
 
+-- | Insert a node into the graph. If a node with the given identifier aready exists, its attribute
+-- is updated. /O(v)/.
 insertNode :: Node -> SymbolicGraph -> SymbolicGraph
 insertNode n =
   modifyGraphPart $ Graph.insertNodeWithPayload (nodeId n) (nodeAttribute n)
 
 
+-- | Insert an edge into the graph. If an edge with the given identifier aready exists, its source
+-- and target are updated. /O(v)/.
 insertEdge :: Edge -> SymbolicGraph -> SymbolicGraph
 insertEdge e =
   modifyGraphPart $ Graph.insertEdgeWithPayload (edgeId e) (sourceId e) (targetId e) ()
@@ -308,11 +354,13 @@ insertEdge e =
 -- * Delete
 
 
+-- | Removes the given node from the graph, unless it has any incident edges. /O(v + e²)/.
 deleteNode :: NodeId -> SymbolicGraph -> SymbolicGraph
 deleteNode n =
   modifyGraphPart (Graph.removeNode n)
 
 
+-- | Remove the given edge from the graph. /O(e)/.
 deleteEdge :: EdgeId -> SymbolicGraph -> SymbolicGraph
 deleteEdge e =
   modifyGraphPart (Graph.removeEdge e)
@@ -322,9 +370,16 @@ deleteEdge e =
 -- * Update
 
 
+-- | Update the node's attribute, applying the given function on it. /O(v)/.
 updateNodeAttribute :: (Maybe Variable -> Maybe Variable) -> NodeId -> SymbolicGraph -> SymbolicGraph
 updateNodeAttribute f nodeId =
   modifyGraphPart (Graph.updateNodePayload f nodeId)
+
+
+-- | Update the attributes of all nodes. /O(v)/.
+updateNodeAttributes :: (Node -> Maybe Variable) -> SymbolicGraph -> SymbolicGraph
+updateNodeAttributes f =
+  modifyGraphPart $ Graph.mapNodes (f . Node)
 
 
 
@@ -332,40 +387,38 @@ updateNodeAttribute f nodeId =
 -- ** Lists
 
 
+-- | List of all nodes from the graph. /O(v)/.
 nodes :: SymbolicGraph -> [Node]
 nodes =
   map Node . Graph.nodes . graphPart
 
 
+-- | List of all edges from the graph. /O(e)/.
 edges :: SymbolicGraph -> [Edge]
 edges =
   map Edge . Graph.edges . graphPart
 
 
+-- | List of all node id's from from the graph. /O(v)/.
 nodeIds :: SymbolicGraph -> [NodeId]
 nodeIds =
   Graph.nodeIds . graphPart
 
 
+-- | List of all edge id's from from the graph. /O(e)/.
 edgeIds :: SymbolicGraph -> [EdgeId]
 edgeIds =
   Graph.edgeIds . graphPart
 
 
+-- | List of all nodes from the graph, along with their contexts. /O(v)/.
 nodesInContext :: SymbolicGraph -> [NodeInContext]
 nodesInContext =
   map wrapNodeInContext . Graph.nodesInContext . graphPart
 
 
+-- | List of all edges from the graph, along with their contexts.
+-- /O(e)/, plus the cost of evaluating the nodes of the result (see 'EdgeInContext').
 edgesInContext :: SymbolicGraph -> [EdgeInContext]
 edgesInContext =
   map wrapEdgeInContext . Graph.edgesInContext . graphPart
-
-
-
--- * Map
-
-
-mapNodeAttributes :: (Node -> Maybe Variable) -> SymbolicGraph -> SymbolicGraph
-mapNodeAttributes f =
-  modifyGraphPart $ Graph.mapNodes (f . Node)
