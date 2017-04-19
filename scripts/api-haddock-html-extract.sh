137d50590388ff7104878ba05c29780142372f6a
Leonardo Rodrigues <lmrodrigues@inf.ufrgs.br>
2017-04-19 14:35:08 -0300

    New script for documentation extraction

--- scripts/api-haddock-html-extract.sh
+++ scripts/api-haddock-html-extract.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # Scripting for generating HTML files for the api documentation of Verigraph.
-# Must be run from the root directory of the verigraph project.
+# Must be run from directory tree of the verigraph project.
 #
 # Compiles and installs verigraph, generate the HTMLs for the api documentation,
 # then, copy the files to doc folder on the root directory of the verigraph project.
@@ -12,11 +12,25 @@
 USAGE="Usage:   $0 "
 [ "$1" == "help" ] && echo $USAGE && exit 0
 
-OUTPUT="./doc"
-rm -r "$OUTPUT"
+#Get the path to the project root
+PROJECT_ROOT_PATH="$(stack path --project-root | tr -d "\n")"
 
-HTML_DIR=".stack-work/dist/x86_64-linux/Cabal-1.24.0.0/doc/html/verigraph"
-rm -r "$HTML_DIR"
-stack build --haddock --force-dirty  && stack install
+#Get the path to the local doc directory
+LOCAL_DOC_PATH="$(stack path --local-doc-root | tr -d "\n")"
 
-cp -r "$HTML_DIR" "$OUTPUT"
+#Get package name and version
+PACKAGE_NAME="$(stack query locals | grep ":$" | tr -d ": | \n")"
+PACKAGE_VERSION="$(stack query locals | grep "version:"| tr -d " *version: | ' | \n")"
+
+#Build the specific package version doc directory
+HTML_DIR="$LOCAL_DOC_PATH/$PACKAGE_NAME-$PACKAGE_VERSION"
+
+
+#Build the specific output directory on the project root
+OUTPUT="$PROJECT_ROOT_PATH/doc"
+
+#Clean the directorys to avoid extract of old files
+rm -r "$HTML_DIR";
+rm -r "$OUTPUT";
+
+stack build --haddock --no-haddock-deps --force-dirty  && stack install  && cp -r "$HTML_DIR" "$OUTPUT"
