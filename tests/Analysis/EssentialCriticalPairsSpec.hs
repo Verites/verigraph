e69f08ca08cadd4b272b4bc0a7b3abd6dd5a8c6a
Andrei Costa <andrei529@msn.com>
2017-05-28 12:00:30 -0300

    Refactoring EssentialCriticalPairs

---
+++ tests/Analysis/EssentialCriticalPairsSpec.hs
@@ -0,0 +1,61 @@
+module Analysis.EssentialCriticalPairsSpec where
+
+import           Data.Matrix                      hiding ((<|>))
+import           Test.Hspec
+
+import           Abstract.Rewriting.DPO
+import           Analysis.EssentialCriticalPairs
+import           Category.TypedGraphRule.EpiPairs ()
+import           Utils
+import qualified XML.GGXReader                    as XML
+
+fileName1 = "tests/grammars/elevator.ggx"
+fileName2 = "tests/grammars/secondOrderMatchTest.ggx"
+dpoConf = MorphismsConfig MonoMatches PartiallyMonomorphicNAC
+testCase findEssentialCP rules expected = expected `shouldBe` show (pairwise (findEssentialCP dpoConf) rules)
+
+spec :: Spec
+spec = do
+  context "Essential Critical Pairs Test"
+    ecpaTest
+
+ecpaTest :: Spec
+ecpaTest = do
+    it "first-order" $ do
+      (gg1,_,_) <- XML.readGrammar fileName1 False dpoConf
+      let fstRules = map snd (productions gg1)
+      
+      testElevatorConflicts fstRules
+    
+    it "second-order" $ do
+      (_,gg2,_) <- XML.readGrammar fileName2 False dpoConf
+      let sndRules = map snd (productions gg2)
+      
+      testSndOrderConflicts sndRules
+
+testSndOrderConflicts rules =
+  do
+    testCase findAllEssentialDeleteUse rules $
+        "( 0 0 0 0 0 )\n"++
+        "( 0 0 0 0 0 )\n"++
+        "( 0 0 2 0 0 )\n"++
+        "( 0 0 0 1 0 )\n"++
+        "( 0 0 0 0 1 )\n"
+
+testElevatorConflicts rules =
+  do
+    testCase findAllEssentialDeleteUse rules $
+        "( 1 1 0 0 1 0 1 0 0 )\n"++
+        "( 0 0 0 0 0 0 0 0 0 )\n"++
+        "( 0 0 0 0 0 0 0 0 0 )\n"++
+        "( 0 0 0 0 0 0 0 0 0 )\n"++
+        "( 0 0 0 0 1 1 1 0 0 )\n"++
+        "( 0 0 0 0 0 1 1 0 1 )\n"++
+        "( 0 0 0 0 1 0 1 0 0 )\n"++
+        "( 0 0 0 0 0 0 0 0 0 )\n"++
+        "( 0 0 0 0 1 1 1 0 4 )\n"
+
+pairwise :: (a -> a -> [b]) -> [a] -> Matrix Int
+pairwise f items =
+  matrix (length items) (length items) $ \(i,j) ->
+    length (f (items !! (i-1)) (items !! (j-1)))
