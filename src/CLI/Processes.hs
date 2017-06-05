340330bd9574a2c671a577ec4ad891cd5ea082e4
Jonas S. Bezerra <jsbezerra@inf.ufrgs.br>
2017-06-05 18:23:48 -0300

    Generating graph processes for all rule sequences in a ggx file

--- src/CLI/Processes.hs
+++ src/CLI/Processes.hs
@@ -43,90 +43,97 @@ execute globalOpts opts = do
     names <- XML.readNames (inputFile globalOpts)
     sequences <- XML.readSequencesWithObjectFlow gg (inputFile globalOpts)
 
-    let colimit = calculateRulesColimit $ head sequences
-        conflictsAndDependencies = findConflictsAndDependencies colimit
-        inducedByNacs = filterInducedByNacs (eliminateSelfConflictsAndDependencies conflictsAndDependencies)
-
-        ogg = generateDoublyTypedGrammar $ head sequences
-        sgg = singleTypedGrammar ogg
-        completeOgg = calculateNacRelations ogg inducedByNacs
-        newRules = productions . singleTypedGrammar $ ogg
-        relation = concreteRelation completeOgg
-        rulesRelation = filterRulesOccurrenceRelation relation
-        elementsRelation = filterElementsOccurrenceRelation relation
-        unique = uniqueOrigin newRules
-        (rulesNames, elementsNames) = getElements completeOgg
-    forM_ (zip sequences newRules) $ \((name, _, _), productions) ->
-      when (null productions)
-        (putStrLn $ "No graph process candidates were found for rule sequence '" ++ name ++ "'")
-
     if null sequences then error "input file must have at least a rule sequence" else print ""
+    forM_ sequences $ \sq -> mainFunction opts sq gg2 ggName names
 
-    let analysis =
-            "Conflicts and Dependencies: {\n"
-            ++ show  (eliminateSelfConflictsAndDependencies conflictsAndDependencies) ++ "\n}\n"
-            ++ "Creation and Deletion Relation: {\n"
-            ++ show (originRelation completeOgg) ++"\n}\n"
-            ++ "Conflicts and dependencies induced by NACs: \n{"
-            ++ show (map (findConcreteTrigger completeOgg) (toList inducedByNacs)) ++ "\n}\n"
+sequenceName :: (String, r, o) -> String
+sequenceName (a,_,_ ) = a
 
-        rulesOrdering = findOrder rulesRelation rulesNames
-        elementsOrdering = findOrder elementsRelation elementsNames
+mainFunction opts sq gg2 ggName names = do
+  let colimit = calculateRulesColimit $ sq
+      conflictsAndDependencies = findConflictsAndDependencies colimit
+      inducedByNacs = filterInducedByNacs (eliminateSelfConflictsAndDependencies conflictsAndDependencies)
+      sqName = sequenceName sq
+      ogg = generateDoublyTypedGrammar $ sq
+      sgg = singleTypedGrammar ogg
+      completeOgg = calculateNacRelations ogg inducedByNacs
+      newRules = productions . singleTypedGrammar $ ogg
+      relation = concreteRelation completeOgg
+      rulesRelation = filterRulesOccurrenceRelation relation
+      elementsRelation = filterElementsOccurrenceRelation relation
+      unique = uniqueOrigin newRules
+      (rulesNames, elementsNames) = getElements completeOgg
+  forM_ (zip [sq] newRules) $ \((name, _, _), productions) ->
+    when (null productions)
+      (putStrLn $ "No graph process candidates were found for rule sequence '" ++ name ++ "'")
 
-        testCases = "Rules involved: {\n"
-          ++ show rulesNames ++ "\n}\n"
-          ++ "Concrete Rules Relation: {\n"
-          ++ relationToString rulesRelation ++ "\n}\n"
-          ++ "Elements involved: {\n"
-          ++ show elementsNames ++ "\n}\n"
-          ++ "Elements Relation: {\n"
-          ++ relationToString elementsRelation ++ "\n}\n"
-          ++ "\n\n"
-          ++ "Rules Ordering: {"
-          ++ show rulesOrdering ++ "\n}\n"
-          ++ "Element Ordering: {"
-          ++ show elementsOrdering ++"\n}\n\n"
-          ++ "Set of Category Restrictions: {\n"
-          ++ restrictionToString (restrictRelation completeOgg) ++ "\n}"
 
-    putStrLn "Testing Serialization: "
-    if unique
-      then putStrLn "[OK] Unique creations and deletions"
-      else putStrLn "[FAIL] At least one element is created or deleted for more than one rule"
 
-    if isValid (initialGraph completeOgg)
-      then putStrLn "[OK] Initial graph is valid"
-      else putStrLn $ "[FAIL] Initial graph is not valid: \n"
-                    ++ fromJust (errorMessages $ validate $ initialGraph completeOgg)
-                    ++ "\n" ++ show (initialGraph completeOgg)
+  let analysis =
+          "Conflicts and Dependencies: {\n"
+          ++ show  (eliminateSelfConflictsAndDependencies conflictsAndDependencies) ++ "\n}\n"
+          ++ "Creation and Deletion Relation: {\n"
+          ++ show (originRelation completeOgg) ++"\n}\n"
+          ++ "Conflicts and dependencies induced by NACs: \n{"
+          ++ show (map (findConcreteTrigger completeOgg) (toList inducedByNacs)) ++ "\n}\n"
 
-    if isValid (finalGraph completeOgg)
-      then putStrLn "[OK] Final graph is valid"
-      else putStrLn $ "[FAIL] Final graph is not valid: \n"
-                    ++ fromJust (errorMessages $ validate $ finalGraph completeOgg)
-                    ++ "\n" ++ show (finalGraph completeOgg)
+      rulesOrdering = findOrder rulesRelation rulesNames
+      elementsOrdering = findOrder elementsRelation elementsNames
 
-    if isJust rulesOrdering
-      then putStrLn "[OK] Concrete occurrence relation is a total order"
-      else putStrLn "[FAIL] Concrete occurrence relation is not a total order"
+      testCases = "Rules involved: {\n"
+        ++ show rulesNames ++ "\n}\n"
+        ++ "Concrete Rules Relation: {\n"
+        ++ relationToString rulesRelation ++ "\n}\n"
+        ++ "Elements involved: {\n"
+        ++ show elementsNames ++ "\n}\n"
+        ++ "Elements Relation: {\n"
+        ++ relationToString elementsRelation ++ "\n}\n"
+        ++ "\n\n"
+        ++ "Rules Ordering: {"
+        ++ show rulesOrdering ++ "\n}\n"
+        ++ "Element Ordering: {"
+        ++ show elementsOrdering ++"\n}\n\n"
+        ++ "Set of Category Restrictions: {\n"
+        ++ restrictionToString (restrictRelation completeOgg) ++ "\n}"
 
-    if isJust elementsOrdering
-      then putStrLn "[OK] Concrete elements relation is a total order"
-      else putStrLn "[FAIL] Concrete elements relation is not a total order"
+  putStrLn $ "\nTesting Serialization for " ++ sqName ++ ":"
+  if unique
+    then putStrLn "[OK] Unique creations and deletions"
+    else putStrLn "[FAIL] At least one element is created or deleted for more than one rule"
 
-    if emptyRestrictions completeOgg
-      then putStrLn "[OK] There are no abstract restrictions"
-      else putStrLn "[WARN] There are abstract restrictions"
+  if isValid (initialGraph completeOgg)
+    then putStrLn "[OK] Initial graph is valid"
+    else putStrLn $ "[FAIL] Initial graph is not valid: \n"
+                  ++ fromJust (errorMessages $ validate $ initialGraph completeOgg)
+                  ++ "\n" ++ show (initialGraph completeOgg)
 
-    writeFile (outputFile opts ++ "_analysis") analysis
-    putStrLn $ "Analysis written in " ++ (outputFile opts ++ "_analysis")
+  if isValid (finalGraph completeOgg)
+    then putStrLn "[OK] Final graph is valid"
+    else putStrLn $ "[FAIL] Final graph is not valid: \n"
+                  ++ fromJust (errorMessages $ validate $ finalGraph completeOgg)
+                  ++ "\n" ++ show (finalGraph completeOgg)
 
-    writeFile (outputFile opts ++ "_test_cases") testCases
-    putStrLn $ "Test cases written in " ++ (outputFile opts ++ "_test_cases")
+  if isJust rulesOrdering
+    then putStrLn "[OK] Concrete occurrence relation is a total order"
+    else putStrLn "[FAIL] Concrete occurrence relation is not a total order"
 
-    let newStart = start sgg
-        gg' = addReachableGraphs (reachableGraphs sgg) (grammar newStart [] newRules)
-    GW.writeGrammarFile (gg',gg2) ggName (buildNewNames names (doubleType completeOgg)) (outputFile opts ++ ".ggx")
+  if isJust elementsOrdering
+    then putStrLn "[OK] Concrete elements relation is a total order"
+    else putStrLn "[FAIL] Concrete elements relation is not a total order"
+
+  if emptyRestrictions completeOgg
+    then putStrLn "[OK] There are no abstract restrictions"
+    else putStrLn "[WARN] There are abstract restrictions"
+
+  writeFile (outputFile opts ++ "_" ++ sqName ++"_analysis") analysis
+  putStrLn $ "Analysis written in " ++ (outputFile opts ++ "_" ++ sqName ++ "_analysis")
+
+  writeFile (outputFile opts ++ "_" ++ sqName ++ "_test_cases") testCases
+  putStrLn $ "Test cases written in " ++ (outputFile opts ++ "_" ++ sqName ++ "_test_cases")
+
+  let newStart = start sgg
+      gg' = addReachableGraphs (reachableGraphs sgg) (grammar newStart [] newRules)
+  GW.writeGrammarFile (gg',gg2) ggName (buildNewNames names (doubleType completeOgg)) (outputFile opts ++ "_" ++ sqName ++ ".ggx")
 
 buildNewNames :: [(String,String)] -> TG.TypedGraph a b -> [(String,String)]
 buildNewNames oldNames tg = newNs ++ newEs
