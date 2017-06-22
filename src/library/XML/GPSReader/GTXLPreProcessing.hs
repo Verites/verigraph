d701e0f4a2b984f61e1609da6c78de881a08bb67
Andrei Costa <andrei529@msn.com>
2017-06-21 21:27:48 -0300

    Reading types

--- src/library/XML/GPSReader/GTXLPreProcessing.hs
+++ src/library/XML/GPSReader/GTXLPreProcessing.hs
@@ -1,53 +1,76 @@
 module XML.GPSReader.GTXLPreProcessing where
 
+import           Data.List
 import           Data.Maybe                    (fromMaybe)
 
 import           XML.GPSReader.GTXLParseIn
 
-data ElementCondition = Creation | Deletion | Preservation deriving(Eq,Show)
+data ElementCondition = Creation | Deletion | Preservation | Forbidden deriving(Eq,Show)
 
 type ProcessedNode = (Id,Id,ElementCondition)
 type ProcessedEdge = (Id,Id,Id,Id,ElementCondition)
 
-nodeType :: Id
-nodeType = 100
-
-edgeType :: Id
-edgeType = 100
-
 getNodeId :: [NodeWithId] -> Node -> Id
-getNodeId nodes node = fromIntegral (fromMaybe (error "node?") (lookup node nodes))
+getNodeId nodes node =
+  fromIntegral
+    (fromMaybe (error ("error\n" ++ show nodes ++" <-> " ++ show node))
+    (lookup node nodes))
 
-processEdges :: [NodeWithId] -> [EdgeWithId] -> [ProcessedEdge]
-processEdges _ [] = []
-processEdges nodes (((from,to,label),id):directedEdges) =
-  (id, sourceId, targetId, edgeType, verify label) : processEdges nodes directedEdges
+processTypeGraph :: [EdgeWithId] -> ([(String,Id)],[(String,Id)])
+processTypeGraph [] = ([],[])
+processTypeGraph (((_,_,label),id):edges) =
+  (if "type:" `isPrefixOf` label
+    then addFst (normalizeLabel label,id)
+    else addSnd (label,id)
+  )
+  (processTypeGraph edges)
+  where
+    addFst z (x,y) = (z:x,y)
+    addSnd z (x,y) = (x,z:y)
+
+processEdges :: [NodeWithId] -> [NodeWithId] -> [EdgeWithId] -> [ProcessedEdge]
+processEdges _ _ [] = []
+processEdges types nodes (((from,to,label),id):directedEdges) =
+  (id, sourceId, targetId, edgeType, verify label) : processEdges types nodes directedEdges
   where
     sourceId = getNodeId nodes from
     targetId = getNodeId nodes to
+    edgeType = getNodeId types (normalizeLabel label)
     verify ('n':'e':'w':':':_) = Creation
     verify ('d':'e':'l':':':_) = Deletion
+    verify ('n':'o':'t':':':_) = Forbidden
     verify _ = Preservation
 
-processNodes :: [NodeWithId] -> [EdgeWithId] -> [EdgeWithId] -> [ProcessedNode]
-processNodes = processPreservedNodes
+processNodes :: [NodeWithId] -> [NodeWithId] -> [EdgeWithId] -> [EdgeWithId] -> [ProcessedNode]
+processNodes types nodes ruleEdges typingEdges =
+  processCreatedDeletedNodes types nodes typingEdges ruleEdges ++
+  processPreservedNodes types nodes ruleEdges typingEdges
 
-processCreatedDeletedNodes :: [NodeWithId] -> [EdgeWithId] -> [ProcessedNode]
-processCreatedDeletedNodes _ [] = []  
-processCreatedDeletedNodes nodes (((node,_,label),_):ruleEdges) =
-  (nodeId, nodeType, verify label) : processCreatedDeletedNodes nodes ruleEdges
+processCreatedDeletedNodes :: [NodeWithId] -> [NodeWithId] -> [EdgeWithId] -> [EdgeWithId] -> [ProcessedNode]
+processCreatedDeletedNodes _ _ _ [] = []
+processCreatedDeletedNodes types nodes typingEdges (((node,_,label),_):ruleEdges) =
+  (nodeId, nodeType, verify label) : processCreatedDeletedNodes types nodes typingEdges ruleEdges
   where
-    nodeId = getNodeId nodes node
+    ((n2,_,l2),_) = head [x | x@((n,_,l),_) <- typingEdges, "type:" `isPrefixOf` l, n == node]
+    
+    nodeId = getNodeId nodes n2
+    nodeType = getNodeId types (normalizeLabel l2)
+    
     verify "new:" = Creation
     verify "del:" = Deletion
-    verify er = error ("verifying unknown token: " ++ er)
+    verify "not:" = Forbidden
+    verify msg = error (show msg) 
 
-processPreservedNodes :: [NodeWithId] -> [EdgeWithId] -> [EdgeWithId] -> [ProcessedNode]
-processPreservedNodes nodes ruleEdges [] = processCreatedDeletedNodes nodes ruleEdges
-processPreservedNodes nodes ruleEdges (((node,_,_),_):typingEdges) =
+processPreservedNodes :: [NodeWithId] -> [NodeWithId] -> [EdgeWithId] -> [EdgeWithId] -> [ProcessedNode]
+processPreservedNodes _ _ _ [] = []
+processPreservedNodes types nodes ruleEdges (((node,_,label),_):typingEdges) =
   if nodeWasCreatedDeleted
-    then processPreservedNodes nodes ruleEdges typingEdges
-    else (nodeId, nodeType, Preservation) : processPreservedNodes nodes ruleEdges typingEdges
+    then processPreservedNodes types nodes ruleEdges typingEdges
+    else (nodeId, nodeType, Preservation) : processPreservedNodes types nodes ruleEdges typingEdges
   where
     nodeId = getNodeId nodes node
+    nodeType = getNodeId types (normalizeLabel label)
     nodeWasCreatedDeleted = Prelude.not (Prelude.null [x | x@((n2,_,_),_) <- ruleEdges, n2 == node])
+
+normalizeLabel :: String -> String
+normalizeLabel label = if ':' `elem` label then tail (dropWhile (/= ':') label) else label
--- src/library/XML/GPSReader/GTXLReader.hs
+++ src/library/XML/GPSReader/GTXLReader.hs
@@ -4,17 +4,14 @@ import qualified Data.List                     as L
 import           System.Directory
 import           System.FilePath
 
+import           Base.Valid
+
 import           Abstract.Rewriting.DPO
 import qualified Data.Graphs                   as G
 import           Data.TypedGraph.Morphism      as TGM
 import           Rewriting.DPO.TypedGraph      as GR
 import           XML.GPSReader.GTXLParseIn
 import           XML.GPSReader.GTXLPreProcessing
-
-type Rule = (RuleName,RuleGraph)
-
-tempTypeGraph :: G.Graph (Maybe n) (Maybe e)
-tempTypeGraph = G.build [100] [(100,100,100)]
 
 readGrammar :: String -> IO ()
 readGrammar fileName = do
@@ -23,34 +20,63 @@ readGrammar fileName = do
   
   --let stateNames = filter (\name -> takeExtension name == ".gst") files
   let ruleNames = filter (\name -> takeExtension name == ".gpr") files
-      ruleFileNames = map (\n -> fileName ++ "/" ++ n) ruleNames
+      typeGraphName = head (filter (\name -> takeExtension name == ".gty") files)
+      
+      rulePathNames = map (\n -> fileName ++ "/" ++ n) ruleNames
+      typeGraphPathName = fileName ++ "/" ++ typeGraphName
+      
+  parsedTypeGraph <- parseGPR typeGraphPathName
   
-  t <- mapM parseGPR ruleFileNames
+  let toProcess = snd . snd
+      processedTypes = processTypeGraph (toProcess parsedTypeGraph)      
+      typeGraph = instatiateTypeGraph parsedTypeGraph
+      
+  --print processedTypes
+
+  parsedRules <- mapM parseGPR rulePathNames
   
-  print (instatiateOneRule (t!!0))
+  --print parsedRules
+  
+  let rules = map (instatiateOneRule typeGraph processedTypes) parsedRules
+  --print (rules!!1)
+  --print typeGraph
+  
+  print rules
+  print (map (Base.Valid.isValid . snd) rules)
   
   return ()
 
-instatiateOneRule :: Rule -> (RuleName, TypedGraphRule a b)
-instatiateOneRule rule = (fst rule, instatiateRule processedNodes processedEdges)
+instatiateTypeGraph :: NamedRuleGraph -> G.Graph (Maybe a) (Maybe b)
+instatiateTypeGraph (_,(_,types)) = G.build nodesToBuild edgesToBuild
+  where
+    (nodes,edges) = L.partition (\((_,_,label),_) -> L.isPrefixOf "type:" label) types
+    nodesToBuild = map snd nodes
+    edgesToBuild = map (\((src,tgt,_),id) -> (id, getNodeType nodes src, getNodeType nodes tgt)) edges
+
+getNodeType :: [EdgeWithId] -> String -> Id
+getNodeType nodes node = head [id | ((name,_,_),id) <- nodes, node == name]
+
+instatiateOneRule :: G.Graph (Maybe a) (Maybe b) -> ([NodeWithId],[NodeWithId]) -> NamedRuleGraph -> (RuleName, TypedGraphRule a b)
+instatiateOneRule typeGraph (nodeTypes,edgeTypes) rule = (fst rule, instatiateRule typeGraph processedNodes processedEdges)
   where
     (nodes,edges) = snd rule
     (selfLoopEdges,directedEdges) = L.partition (\((from,to,_),_) -> from == to) edges
     (typingEdges,ruleEdges) = L.partition (\((_,_,label),_) -> L.isPrefixOf "type:" label) selfLoopEdges
     
-    processedNodes = processNodes nodes ruleEdges typingEdges
-    processedEdges = processEdges nodes directedEdges
+    processedNodes = processNodes nodeTypes nodes ruleEdges typingEdges
+    processedEdges = processEdges edgeTypes nodes directedEdges
 
-instatiateRule :: [ProcessedNode] -> [ProcessedEdge] -> TypedGraphRule a b
-instatiateRule nodes = instatiateRuleEdges (instatiateRuleNodes nodes)
+instatiateRule :: G.Graph (Maybe a) (Maybe b) -> [ProcessedNode] -> [ProcessedEdge] -> TypedGraphRule a b
+instatiateRule typeGraph nodes = instatiateRuleEdges (instatiateRuleNodes typeGraph nodes)
 
-instatiateRuleNodes :: [ProcessedNode] -> TypedGraphRule a b
-instatiateRuleNodes [] = emptyGraphRule tempTypeGraph
-instatiateRuleNodes ((n,ntype,cond):nodes) = action cond (instatiateRuleNodes nodes)
+instatiateRuleNodes :: G.Graph (Maybe a) (Maybe b) -> [ProcessedNode] -> TypedGraphRule a b
+instatiateRuleNodes typeGraph [] = emptyGraphRule typeGraph
+instatiateRuleNodes typeGraph ((n,ntype,cond):nodes) = action cond (instatiateRuleNodes typeGraph nodes)
   where
     action Preservation = addsPreservationNode n ntype
     action Creation = addsCreationNode n ntype
     action Deletion = addsDeletionNode n ntype
+    action Forbidden = Prelude.id
 
 instatiateRuleEdges :: TypedGraphRule a b -> [ProcessedEdge] -> TypedGraphRule a b
 instatiateRuleEdges init [] = init
@@ -59,6 +85,7 @@ instatiateRuleEdges init ((e,src,tgt,etype,cond):edges) = action cond (instatiat
     action Preservation = addsPreservationEdge e src tgt etype
     action Creation = addsCreationEdge e src tgt etype
     action Deletion = addsDeletionEdge e src tgt etype
+    action Forbidden = Prelude.id
 
 addsDeletionNode :: Id -> Id -> TypedGraphRule a b -> TypedGraphRule a b
 addsDeletionNode nodeId ntype r =
