5d8e2a7cdc1c99c1dd5e98e6089a783a90a0cd4e
Andrei Costa <andrei529@msn.com>
2017-06-25 11:09:15 -0300

    Added Mutex grammar to the GROOVE Reader tests

--- tests/XML/GPRReader/GXLReaderSpec.hs
+++ tests/XML/GPRReader/GXLReaderSpec.hs
@@ -13,17 +13,31 @@ import qualified XML.GPRReader.GXLReader          as GPR
 
 fileName1 = "tests/grammars/pacman2.ggx"
 fileName2 = "tests/grammars/pacman.gps"
+fileName3 = "tests/grammars/mutex2.ggx"
+fileName4 = "tests/grammars/mutex.gps"
 
 dpoConf = MorphismsConfig AnyMatches MonomorphicNAC
 
 spec :: Spec
-spec = context "GPR Reader Test" gprTest
+spec = context "GPR Reader Test - CPA/CSA analysis is equal on GGX and GPR files" gprTest
 
 gprTest :: Spec
 gprTest = do
-    it "Pacman grammar CPA analysis must be equal on GGX and GPR files" $ do
+    it "Pacman grammar" $ do
       (ggGGX,_,_) <- XML.readGrammar fileName1 False dpoConf
       (ggGPR,_) <- GPR.readGrammar fileName2
+      
+      let rulesGGX = map snd (sortRules (productions ggGGX))
+          rulesGPR = map snd (sortRules (productions ggGPR))
+          
+          sortRules = sortBy (\(a,_) (b,_) -> compare a b)
+      
+      (pairwise (findCriticalPairs dpoConf) rulesGPR) `shouldBe` pairwise (findCriticalPairs dpoConf) rulesGGX
+      (pairwise (findCriticalSequences dpoConf) rulesGPR) `shouldBe` pairwise (findCriticalSequences dpoConf) rulesGGX
+
+    it "Mutex grammar" $ do
+      (ggGGX,_,_) <- XML.readGrammar fileName3 False dpoConf
+      (ggGPR,_) <- GPR.readGrammar fileName4
       
       let rulesGGX = map snd (sortRules (productions ggGGX))
           rulesGPR = map snd (sortRules (productions ggGPR))
