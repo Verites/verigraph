cbd61b5309301bb2a30c160b46d0b713d6419440
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2017-08-03 16:56:17 -0300

    Migrate Category.TypedGraphRule [skip ci]

--- src/library/Rewriting/DPO/TypedGraphRule/Scheduling.hs
+++ src/library/Rewriting/DPO/TypedGraphRule/Scheduling.hs
@@ -8,82 +8,86 @@ module Rewriting.DPO.TypedGraphRule.Scheduling
   , specific
   ) where
 
-import           Data.Maybe                   (fromMaybe, isNothing)
+import           Data.Maybe                   (fromMaybe)
 
-import           Abstract.Rewriting.DPO       (MorphismsConfig,findApplicableMatches,rewrite)
-import           Category.TypedGraphRule      (RuleMorphism)
+import           Abstract.Rewriting.DPO       (findApplicableMatches,rewrite)
+import           Category.TypedGraphRule      
 import           Rewriting.DPO.TypedGraph     (TypedGraphRule)
 import           Rewriting.DPO.TypedGraphRule (SndOrderRule)
 
 -- second-order rule is synonymous of 2-rule.
 -- first-order rule is synonymous of rule.
-type NamedRule a b = (String,TypedGraphRule a b)
-type Named2Rule a b = (String,SndOrderRule a b)
+type NamedRule n e = (String,TypedGraphRule n e)
+type Named2Rule n e = (String,SndOrderRule n e)
 
 -- | Apply "AsLongAsPossible" any second-order rule.
 -- It is limited to `n` rewritings.
-asLongAsPossible :: MorphismsConfig -> [Named2Rule a b] -> [NamedRule a b] -> Int -> (String,[NamedRule a b])
-asLongAsPossible c s f n = asLongAsPossible_ c s f ("limit of rewritings: " ++ show n) n
+asLongAsPossible :: [Named2Rule n e] -> [NamedRule n e] -> Int -> TGRuleCat n e (String,[NamedRule n e])
+asLongAsPossible s f n = asLongAsPossible_ s f ("limit of rewritings: " ++ show n) n
 
 -- Each iteration applies a rewrite, removes the old rule, and put the new rule in the end of the rules list.
-asLongAsPossible_ :: MorphismsConfig -> [Named2Rule a b] -> [NamedRule a b] -> String -> Int -> (String,[NamedRule a b])
-asLongAsPossible_ _ _ fstRules log 0 = (log ++ "\n limit of rewritings exceeded.",fstRules) 
-asLongAsPossible_ conf sndRules fstRules log n =
-  if isNothing matches
-    then (log++"\nno more matches.",fstRules)
-    else asLongAsPossible_ conf sndRules newFstOrder newLog (n-1)
-  where
-    newLog = log ++ "\n " ++ r1Name ++ " --" ++ fst r2 ++ "--> " ++ newName ++ "."
-    matches = getOneMatch conf sndRules [] fstRules
-    (Just (m,r2,r1Name,oldRules)) = matches
-    newName = r1Name ++ "_" ++ fst r2
-    newRule = rewrite m (snd r2)
-    newFstOrder = oldRules ++ [(newName,newRule)]
+asLongAsPossible_ :: [Named2Rule n e] -> [NamedRule n e] -> String -> Int -> TGRuleCat n e (String,[NamedRule n e])
+asLongAsPossible_ _ fstRules log 0 = return (log ++ "\n limit of rewritings exceeded.",fstRules) 
+asLongAsPossible_ sndRules fstRules log n = do
+  matches <- getOneMatch sndRules [] fstRules
+  case matches of
+    Nothing -> return (log++"\nno more matches.",fstRules)
+    Just (m,r2,r1Name,oldRules) -> do
+      let newName = r1Name ++ "_" ++ fst r2
+          newLog = log ++ "\n " ++ r1Name ++ " --" ++ fst r2 ++ "--> " ++ newName ++ "."
+      newRule <- rewrite m (snd r2)
+      let newFstOrder = oldRules ++ [(newName,newRule)]
+      asLongAsPossible_ sndRules newFstOrder newLog (n-1)
 
 -- Returns just one match (if it exits) between [2-rules] and [rules].
 -- Also, returns the list of rules without the matched one.
-getOneMatch :: MorphismsConfig -> [Named2Rule a b] -> [NamedRule a b] -> [NamedRule a b] -> Maybe (RuleMorphism a b, Named2Rule a b, String, [NamedRule a b])
-getOneMatch _ _ _ [] = Nothing
-getOneMatch conf sndRules rs1 (r:rs2) =
-  let matches = getOneMatch_ conf r sndRules
-      (Just (m,r2,r1Name)) = matches
-  in  if isNothing matches then getOneMatch conf sndRules (r:rs1) rs2 else Just (m,r2,r1Name,rs1 ++ rs2)
+getOneMatch :: [Named2Rule n e] -> [NamedRule n e] -> [NamedRule n e] -> TGRuleCat n e (Maybe (RuleMorphism n e, Named2Rule n e, String, [NamedRule n e]))
+getOneMatch _ _ [] = return Nothing
+getOneMatch sndRules rs1 (r:rs2) = do
+  match <- getOneMatch_ r sndRules
+  case match of 
+    Nothing -> getOneMatch sndRules (r:rs1) rs2
+    Just (m,r2,r1Name) -> return (Just (m,r2,r1Name,rs1 ++ rs2))
 
-getOneMatch_ :: MorphismsConfig -> NamedRule a b -> [Named2Rule a b] -> Maybe (RuleMorphism a b, Named2Rule a b, String)
-getOneMatch_ _ _ [] = Nothing
-getOneMatch_ conf rule (r2:r2s) =
-  let matches = findApplicableMatches conf (snd r2) (snd rule)
-  in  if Prelude.null matches then getOneMatch_ conf rule r2s else Just (head matches, r2, fst rule)
+getOneMatch_ :: NamedRule n e -> [Named2Rule n e] -> TGRuleCat n e (Maybe (RuleMorphism n e, Named2Rule n e, String))
+getOneMatch_ _ [] = return Nothing
+getOneMatch_ rule (r2:r2s) = do
+  matches <- findApplicableMatches (snd r2) (snd rule)
+  case matches of
+    [] -> getOneMatch_ rule r2s
+    m:_ ->  return $ Just (m, r2, fst rule)
 
 -- | Apply "oneStep".
 -- All matches from 2-rules to rules are applied once.
-oneStep :: MorphismsConfig -> [Named2Rule a b] -> [NamedRule a b] -> (String,[NamedRule a b])
-oneStep conf sndRules fstRules = (logs,rules)
-  where
-    info = map (oneStep_ conf sndRules) fstRules
+oneStep :: [Named2Rule n e] -> [NamedRule n e] -> TGRuleCat n e (String,[NamedRule n e])
+oneStep sndRules fstRules = do
+  info <- mapM (oneStep_ sndRules) fstRules
+  let
     logs = concatMap fst info
     rules = concatMap snd info
+  return (logs,rules)
 
-oneStep_ :: MorphismsConfig -> [Named2Rule a b] -> NamedRule a b -> (String,[NamedRule a b])
-oneStep_ conf sndRules rule = (logs,rules)
-  where
-    info = map (applyAllMatches conf rule) sndRules
+oneStep_ :: [Named2Rule n e] -> NamedRule n e -> TGRuleCat n e (String,[NamedRule n e])
+oneStep_ sndRules rule = do
+  info <- mapM (applyAllMatches rule) sndRules
+  let
     logs = concatMap fst info
     rules = concatMap snd info
+  return (logs,rules)
 
-applyAllMatches :: MorphismsConfig -> NamedRule a b -> Named2Rule a b -> (String,[NamedRule a b])
-applyAllMatches conf r1 r2 = (log,namedRules)
-  where
-    matches = findApplicableMatches conf (snd r2) (snd r1)
-    newRules = map (`rewrite` snd r2) matches
-    newRulesId = zip newRules ([0..]::[Int])
-    namedRules = map (\(r,id) -> (fst r1 ++"_"++ fst r2 ++"_"++show id,r)) newRulesId
+applyAllMatches :: NamedRule n e -> Named2Rule n e -> TGRuleCat n e (String,[NamedRule n e])
+applyAllMatches r1 r2 = do
+  matches <- findApplicableMatches (snd r2) (snd r1)
+  newRules <- mapM (`rewrite` snd r2) matches
+  let
+    namedRules = zipWith (\r id -> (fst r1 ++"_"++ fst r2 ++"_"++show (id :: Int), r)) newRules [0..]
     log = concatMap (\(name,_) -> "\n "++ fst r1 ++" --"++ fst r2 ++"--> "++name++".") namedRules
+  return (log, namedRules)
 
 -- | Apply "Specific" from a 2rule to a rule.
 -- Receives a 2-rule name and a rule name, and rewrites all matches between them.
-specific :: MorphismsConfig -> [Named2Rule a b] -> [NamedRule a b] -> String -> String -> (String,[NamedRule a b])
-specific conf sndRules fstRules name2Rule nameRule = applyAllMatches conf (nameRule,rule1) (name2Rule,rule2)
+specific :: [Named2Rule n e] -> [NamedRule n e] -> String -> String -> TGRuleCat n e (String,[NamedRule n e])
+specific sndRules fstRules name2Rule nameRule = applyAllMatches (nameRule,rule1) (name2Rule,rule2)
   where
     rule1 = getRule fstRules nameRule
     rule2 = getRule sndRules name2Rule
