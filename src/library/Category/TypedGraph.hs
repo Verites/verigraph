071fa4f8e1421654cdc258b776e89eaa73ae333f
Guilherme G. Azzi <ggazzi@inf.ufrgs.br>
2017-11-14 17:25:15 -0200

    Instantiate Isomorphic for typed graphs

--- src/library/Category/TypedGraph.hs
+++ src/library/Category/TypedGraph.hs
@@ -1,7 +1,16 @@
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE ScopedTypeVariables  #-}
+{-# LANGUAGE TypeApplications     #-}
+{-# LANGUAGE TypeSynonymInstances #-}
 module Category.TypedGraph (TypedGraph, TypedGraphMorphism) where
 
+import           Abstract.Category.FindMorphism
+import           Base.Isomorphic
 import           Category.TypedGraph.Adhesive     ()
 import           Category.TypedGraph.Category
 import           Category.TypedGraph.FindMorphism ()
 import           Category.TypedGraph.Finitary     ()
 import           Category.TypedGraph.Limit        ()
+
+instance {-# OVERLAPS #-} Iso (TypedGraph n e) where
+  g ~= h = not . null $ findIsomorphisms @(TypedGraphMorphism n e) g h
