4ae951d3db7e1ca5b7eb60da2ea3215e125d4b0d
Leonardo Rodrigues <lmrodrigues@inf.ufrgs.br>
2018-03-22 11:09:36 -0300

    Add basic documentation to PartialMapClassifier type class

--- src/library/Abstract/Category/PartialMapClassifier.hs
+++ src/library/Abstract/Category/PartialMapClassifier.hs
@@ -1,9 +1,23 @@
 {-# LANGUAGE AllowAmbiguousTypes #-}
 {-# LANGUAGE DefaultSignatures   #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE TypeApplications    #-}
 
--- |
+-- | This Type Class implements partial map classifiers following
+--   the construction of a stable system of monos, as described on
+--   AGREE -- Algebraic Graph Rewriting with Controlled Embedding (Long Version)
+--   available at https://arxiv.org/abs/1411.4597v2
+--
+--   Category C has an M-Partial Map Classifier if the following diagram are satisfied on a stable system of monos M of C:
+--
+--          f
+--     X ──────▶ Y
+--     V         V
+--   m │   PB    │ ny = partialMapClassifier
+--     ▼         ▼
+--     Z ──────▶ T(Y)
+--       p(m,f) = totalizePartialMap
+
 
 module Abstract.Category.PartialMapClassifier where
 
